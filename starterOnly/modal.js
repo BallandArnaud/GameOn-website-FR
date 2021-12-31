994aa64e50aa545d1ff3b5ab678464b1d30b948c
Arnaud Balland <balland.arnaud@gmail.com>
2021-12-31 11:02:43 +0100

    Fix issue #2 - Implémenter entrées du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,3 +30,50 @@ function closeModal() {
 }
 
 
+function validate(event){
+  //Prevent to submit form
+  event.preventDefault();
+
+  const formFirst = document.getElementById('first'); // Get firstname input
+  const formLast = document.getElementById('last'); // Get lastname input
+  const formEmail = document.getElementById('email'); // Get email input
+  const formBirthdate = document.getElementById('birthdate'); // Get birthdate input
+  const formQuantity = document.getElementById('quantity'); // Get quantity of number of tournament participated input
+  const formLocation = document.querySelector('input[name="location"]:checked'); // Get location of next tournament input radio check
+  const formTermsConditions = document.getElementById('checkbox1'); // Get terms conditions input checkbox
+
+  const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if(formFirst.value == "" || formFirst.value.length < 2){
+    console.log('Problème firstName');
+    return false;
+  }
+
+  if(formLast.value == "" || formLast.value.length < 2){
+    console.log('Problème lastname');
+    return false;
+  }
+
+  if (regexEmail.test(formEmail.value) != true){
+    console.log('Problème adresse email');
+    return false;
+  }
+
+  if(isNaN(formQuantity.value)){
+    console.log("Problème quantité tournois n'est pas un nombre");
+    return false;
+  }
+
+  if(formLocation == null){
+    console.log('Problème aucune location est cochée');
+    return false;
+  }
+
+  if(!formTermsConditions.checked){
+    console.log('Problème la case conditions générales est non cochée');
+    return false;
+  }
+
+  console.log("Inscription envoyée");
+  return true;
+}
