de5e6f7970a08cf12cef3ce838b8c9aacdca2686
Arnaud Balland <balland.arnaud@gmail.com>
2022-01-05 16:44:07 +0100

    Fix issue #5 - Tests manuels

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,6 +27,7 @@ let formLocationCheck = document.querySelector('input[name="location"]:checked')
 const formTermsConditions = document.getElementById('checkbox1'); // Get terms conditions input checkbox
 
 const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const regexName = /^[a-z ,.'-]+$/;
 let formIsValid; // initialize form validation;
 
 // launch modal event
@@ -55,6 +56,7 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// restart modal form
 function restartModal(){
   modalbg.classList.remove('formSubmitted');
   modalBody.style.opacity = "1";
@@ -67,37 +69,39 @@ function restartModal(){
   formLocationCheck.checked = false;
 }
 
-// Ajoute un message d'erreur
+// Add error message
 function addFormErrorMessage(element, errorMessage){
   element.parentElement.setAttribute('data-error', errorMessage);
   element.parentElement.setAttribute('data-error-visible', 'true');
 }
 
-// Supprime un message d'erreur
+// Remove error message
 function removeFormErrorMessage(element){
     element.parentElement.removeAttribute('data-error');
     element.parentElement.removeAttribute('data-error-visible');
 }
 
-
+// Check the validation of the firstname input
 function formFirstIsValid(){
-  if(formFirst.value == "" || formFirst.value.length < 2){
-    addFormErrorMessage(formFirst, "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+  if(formFirst.value == "" || formFirst.value.length < 2 || regexName.test(formFirst.value) != true){
+    addFormErrorMessage(formFirst, "Veuillez entrer 2 caractères valide ou plus pour le champ du prénom.");
     formIsValid = false;
   } else{
     removeFormErrorMessage(formFirst);
   }
 }
 
+// Check the validation of the lastname input
 function formLastIsValid(){
-  if(formLast.value == "" || formLast.value.length < 2){
-    addFormErrorMessage(formLast, "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+  if(formLast.value == "" || formLast.value.length < 2 || regexName.test(formLast.value) != true){
+    addFormErrorMessage(formLast, "Veuillez entrer 2 caractères valide ou plus pour le champ du nom.");
     formIsValid = false;
   } else{
     removeFormErrorMessage(formLast);
   }
 }
 
+// Check the validation of the email input
 function formEmailIsValid(){
   if (regexEmail.test(formEmail.value) != true){
     addFormErrorMessage(formEmail, "Veuillez entrer une adresse email valide.");
@@ -107,15 +111,20 @@ function formEmailIsValid(){
   }
 }
 
+// Check the validation of the birthdate input
 function formBirthdateIsValid(){
   if(formBirthdate.value == ""){
     addFormErrorMessage(formBirthdate, "Vous devez entrer votre date de naissance.");
+    formIsValid = false;
+  } else if (Date.parse(formBirthdate.value) > Date.now()){
+    addFormErrorMessage(formBirthdate, "Vous devez entrer une date de naissance valide.");
     formIsValid = false;
   } else {
     removeFormErrorMessage(formBirthdate);
   }
 }
 
+// Check the validation of the quantity input
 function formQuantityIsValid(){
   if(formQuantity.value == "" || isNaN(formQuantity.value)){
     addFormErrorMessage(formQuantity, "Veuillez entrer un nombre.");
@@ -125,6 +134,7 @@ function formQuantityIsValid(){
   }
 }
 
+// Check the validation of the location input
 function formLocationIsValid(){
   if(formLocationCheck == null){
     addFormErrorMessage(formLocation, "Vous devez choisir une option.");
@@ -134,6 +144,7 @@ function formLocationIsValid(){
   }
 }
 
+// Check the validation of the terms conditions input
 function formTermsConditionsIsValid(){
   if(!formTermsConditions.checked){
     addFormErrorMessage(formTermsConditions, "Vous devez vérifier que vous acceptez les termes et conditions.");
@@ -143,7 +154,7 @@ function formTermsConditionsIsValid(){
   }
 }
 
-// Valide toutes les informations avant envoi
+// Check the validation of the form and return a message if it's ok
 function validate(event){
   //Prevent to submit form
   event.preventDefault();
